592cff3b7b582e6e080cef9777d95adf200498c2
isa <5445946+icortes@users.noreply.github.com>
2021-10-30 15:07:04 -0700

    made comment model for blog posts

---
+++ models/Comment.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize/types');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    },
+    content: {
+      type: DataTypes.STRING
+    },
+    blog_post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'blog_post',
+        key: 'id'
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+  }
+);
+
+module.exports = Comment;
