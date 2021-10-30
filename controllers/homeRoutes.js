daca810d0b06a166625cd122b2b432e2732545d6
isa <5445946+icortes@users.noreply.github.com>
2021-10-30 15:31:49 -0700

    added code to show blogposts on homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,20 @@
+const { BlogPost, User } = require('../models');
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
   try {
-    res.render('homepage');
+    const blogPostData = await BlogPost.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name']
+        }
+      ]
+    });
+
+    // Serialize data so the template can read it
+    const blogPosts = blogPostData.map((blogPost) => blogPost.get({plain: true}));
+    res.render('homepage', blogPosts);
   } catch (error) {
     res.status(500).json(error);
   }
