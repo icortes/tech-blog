379f244729e80190fe515744229a8b559d25a4a0
isa <5445946+icortes@users.noreply.github.com>
2021-10-30 23:49:46 -0700

    made login code for login page

--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,16 @@
 const loginEventHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const email = document.querySelector('#signInEmail').value.trim();
-    const password = document.querySelector('#signInPassword').value.trim();
+  const email = document.querySelector('#signInEmail').value.trim();
+  const password = document.querySelector('#signInPassword').value.trim();
 
-    if(email && password){
-        
-    }
-}
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'content-Type': 'application/json' }
+    });
+
+    response.ok ? document.location.replace('/') : alert(response.statusText);
+  }
+};
